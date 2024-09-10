5156a9ddf36713daa125b451c899404c72fd8c95
Simon Gravelle <simon.gravelle@live.fr>
2024-09-10 13:52:41 +0200

    improved footers

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,16 +13,18 @@ exclude_patterns = []
 # These are options specifically for the Wagtail Theme.
 html_theme_options = dict(
     project_name = "PSM tips",
-    github_url = "https://github.com/psm-compute/psm-compute.github.io/tree/main/docs/source/"
-    # logo_alt = "Wagtail",
-    # logo_height = 59,
-    # logo_url = "/",
-    # logo_width = 45,
+    github_url = "https://github.com/psm-compute/psm-compute.github.io/tree/main/docs/source/",
+    footer_links = ",".join([
+        "LIPhy|https://liphy.univ-grenoble-alpes.fr",
+        "PSM|https://liphy.univ-grenoble-alpes.fr/fr/recherche/equipes/psm-physique-statistique-et-modelisation",
+        "AUTHORS|https://github.com/psm-compute/psm-compute.github.io/blob/main/AUTHORS.md",
+    ]),
 )
 
 html_theme = 'sphinx_wagtail_theme'
 html_static_path = ['_static']
-html_title = "PSM tips and trics page"
+html_title = "PSM tips and tricks page"
 
 html_show_copyright = False
 html_show_sphinx = False
+
